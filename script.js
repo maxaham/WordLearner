926f0872d35f3bd8c103fb3b5815bbe72a0b871c
Max Hamilton <maxaham@gmail.com>
2021-10-16 20:48:16 -0600

    Final

--- script.js
+++ script.js
@@ -1,48 +1,29 @@
-document.getElementById("weatherSubmit").addEventListener("click", function(event) {
+document.getElementById("wordSubmit").addEventListener("click", function(event) {
     event.preventDefault();
-    const value = document.getElementById("weatherInput").value;
+    const value = document.getElementById("wordInput").value;
     if (value === "")
       return;
     console.log(value);
 
-  const url = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=5e8de217ef403ca04b67ebbff0fabe2b";
+  const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + value;
   fetch(url)
-    .then(function(response) {
-      return response.json();
+  .then(function(response) 
+    {return response.json(); 
     }).then(function(json) {
-      console.log(json);
-      let results = "";
-      results += '<div id="forecastNow"><h2>Weather in ' + json.name + "</h2>";
-      for (let i=0; i < json.weather.length; i++) {
-	    results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
+        console.log(json);
+        //console.log(json[0].phonetics[0].text);
+        let results = "";
+        results += '<div id="chosenWord"><h1>' + json[0].word + "</h1>";
+        results += "<h3>"
+        results += "<p>" + json[0].phonetics[0].text + "</p>";
+        //console.log(json.length);
+        for (let i = 0; i < json.length; i++) {
+            //results += "<p>Part of speech: " + json[0].meanings[i].partOfSpeech + "</p>";
+            results += "<p>[" + (i+1) +"] " + json[0].meanings[i].partOfSpeech + ": " +json[0].meanings[i].definitions[0].definition + "</p>";
       }
-      results += '<h2>' + json.main.temp + " &deg;F</h2>"
-      results += "<p>"
-      results += "<p>Feels Like: " + json.main.feels_like + "</p>";
-      results += "<p>Wind | Speed: " + json.wind.speed + " mph - Direction: " + json.wind.deg + " degrees</p>";
-      for (let i=0; i < json.weather.length; i++) {
-	    results += json.weather[i].description
-	    if (i !== json.weather.length - 1)
-	    results += ", "
-        }
-        results += "</p></div>";
-        document.getElementById("weatherResults").innerHTML = results;
-    });
-
-    const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=5e8de217ef403ca04b67ebbff0fabe2b";
-  fetch(url2)
-    .then(function(response) {
-      return response.json();
-    }).then(function(json) {
-      //console.log(json);
-      let forecast = "";
-      for (let i=0; i < json.list.length; i++) { //json.list.length
-        forecast += "<div id='forecastOne'><h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm a') + "</h2>";
-        forecast += "<p>Temperature: " + json.list[i].main.temp + "</p>";
-        forecast += "<p>Feels Like: " + json.list[i].main.feels_like + "</p>";
-        forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/></div>'
-    }
-      document.getElementById("forecastResults").innerHTML = forecast;
+        results += "</h3></div>";
+        //results += "</div>";
+        document.getElementById("wordResults").innerHTML = results;
     });
 
 });
